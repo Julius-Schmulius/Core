5f1d6d97e494ac3bb7ff066def5030001c10e9f2
Schmulius <116003056+Julius-Schmulius@users.noreply.github.com>
2025-12-13 04:25:07 +0100

    rename "Sveltetest" to "componentconfiguration"

--- Console/BExIS.Web.Shell/Areas/DCM/BExIS.Modules.Dcm.UI.Svelte/src/routes/+page.svelte
+++ Console/BExIS.Web.Shell/Areas/DCM/BExIS.Modules.Dcm.UI.Svelte/src/routes/+page.svelte
@@ -18,6 +18,6 @@
 		<a class="btn variant-filled-secondary" href="./create">create</a>
 		<a class="btn variant-filled-secondary" href="./edit">edit</a>
 		<a class="btn variant-filled-secondary" href="./view">view</a>
-		<a class="btn variant-filled-secondary" href="./Sveltetest">Sveltetest</a>
+		<a class="btn variant-filled-secondary" href="./componentconfiguration">Component Configuration</a>
 	</div>
 </Page>
